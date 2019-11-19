0034818d331d50b5b922765e427617dfc3c2fc87
MaorunZhang <504371329@qq.com>
2019-11-19 19:21:04 +0800

    homework

--- src/main/java/com/test/jacoco/HelloWorld.java
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -50,6 +50,14 @@ public class HelloWorld {
     	 * TODO: You need to complete this method to determine whether  a
     	 * triangle is formed or not when given the input edge a, b and c.
     	 */
+    	if(a <= 0 || b <= 0 || c <= 0)
+    		return false;
+    	if(a + b <= c)
+    		return false;
+    	if(a + c <= b)
+    		return false;
+    	if(c + b <= a)
+    		return false;
     	return true;
     }
     public boolean isBirthday(int year, int month, int day) {
@@ -58,6 +66,31 @@ public class HelloWorld {
     	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
     	 * formed or not when given the input year, month and day.
     	 */
+    	if(year < 1990 || year > 2019)
+    	    return false;
+    	if(year == 2019 && month >= 10)
+    	    return false;
+    	if(month < 1 || month > 12)
+    	    return false;
+    	boolean runYear = false;
+
+    	if(year % 4 == 0) {
+    	    runYear = true;
+    	}
+        int totalDays = 0;
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            totalDays = 31;
+        else if(month == 4 || month == 6 || month == 9 || month == 11)
+            totalDays = 30;
+        else
+        {
+            if(runYear)
+                totalDays = 29;
+            else
+                totalDays = 28;
+        }
+        if(day < 1 || day > totalDays)
+            return false;
     	return true;
     	
     }
@@ -68,6 +101,20 @@ public class HelloWorld {
     	 * operation: "+","-","*","/". 
     	 */
     	double result = 0.0;
+    	if(op == '+'){
+    	    result = a+b;
+        }
+        else if(op == '-'){
+            result = a-b;
+        }
+        else if(op == '*'){
+            result = a*b;
+        }
+        else if(op == '/'){
+            if(b != 0){
+                result = a/b;
+            }
+        }
     	return result;
     }
     
--- src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -31,9 +31,122 @@ public class HelloWorldTest {
     	 */
     	HelloWorld hw = new HelloWorld();
     	int a = hw.Method3(6, -1, 1);
-    	
+    	a = hw.Method3(6, 0, 1);
+    	a = hw.Method3(5, -1, 1);
+    	a = hw.Method3(5,0,1);
+        a = hw.Method3(6,-1,0);
+        a = hw.Method3(6,0,0);
+        a = hw.Method3(5,-1,0);
+        a = hw.Method3(5,0,0);
+        a = hw.Method3(4,0,-3);
+        a = hw.Method3(4,0,-2);
+        a = hw.Method3(5,0,-3);
+        a = hw.Method3(5,0,-2);
     }
     /**
      * TODO: add the test function of other methods in HelloWorld.java
      */
+
+    @Test
+    public void testBug(){
+        HelloWorld hw = new HelloWorld();
+        boolean a = hw.bug(5);
+        a = hw.bug(4);
+    }
+
+    @Test
+    public void testMethod4(){
+        HelloWorld hw = new HelloWorld();
+        int a = hw.Method4(0, 1,2,3,4);
+        a = hw.Method4(1,1,2,3,4);
+        //a = hw.Method4(1,2,2,2,4);
+        a = hw.Method4(1,1,2,2,4);
+        //a = hw.Method4(1,2,2,3,4);
+        a = hw.Method4(5,5,2,2,4);
+        a = hw.Method4(5,5,2,3,4);
+        a = hw.Method4(5,4,2,2,4);
+        a = hw.Method4(0,0,2,2,4);
+        a = hw.Method4(0,0,2,3,4);
+        a = hw.Method4(0,1,2,2,4);
+    }
+
+
+    @Test
+    public void testIsTriangle(){
+        HelloWorld hw = new HelloWorld();
+        boolean flag = hw.isTriangle(0,0,0);
+        flag = hw.isTriangle(-1,-2,-3);
+        flag = hw.isTriangle(1,2,3);
+        flag = hw.isTriangle(3,1,2);
+        flag = hw.isTriangle(2,3,1);
+        flag = hw.isTriangle(3,4,5);
+        flag = hw.isTriangle(0,0,1);
+        flag = hw.isTriangle(0,1,0);
+        flag = hw.isTriangle(1,0,0);
+        flag = hw.isTriangle(1,1,0);
+        flag = hw.isTriangle(1,0,1);
+        flag = hw.isTriangle(0,1,1);
+    }
+
+    @Test
+    public void testIsBirthday(){
+        HelloWorld hw = new HelloWorld();
+        boolean flag = hw.isBirthday(1989, 1,1);
+        flag = hw.isBirthday(2020, 1,1);
+        flag = hw.isBirthday(2019, 10,1);
+        flag = hw.isBirthday(2018, 0,1);
+        flag = hw.isBirthday(2018, 13,1);
+        flag = hw.isBirthday(2018, 0,1);
+        flag = hw.isBirthday(2000, 2,29);
+        flag = hw.isBirthday(2001, 2,29);
+        flag = hw.isBirthday(2004, 2,29);
+        flag = hw.isBirthday(2004,1,31);
+        flag = hw.isBirthday(2004, 4,30);
+        flag = hw.isBirthday(2019,9,30);
+        flag = hw.isBirthday(2019, 9,31);
+        flag = hw.isBirthday(1989, 0,1);
+        flag = hw.isBirthday(1989,1,32);
+        flag = hw.isBirthday(2016, 1,0);
+        flag = hw.isBirthday(2016, 1,32);
+        flag = hw.isBirthday(2016, 2,0);
+        flag = hw.isBirthday(2016, 2,29);
+        flag = hw.isBirthday(2016, 3,0);
+        flag = hw.isBirthday(2016, 3,32);
+        flag = hw.isBirthday(2016, 4,0);
+        flag = hw.isBirthday(2016, 4,31);
+        flag = hw.isBirthday(2016, 5,0);
+        flag = hw.isBirthday(2016, 5,32);
+        flag = hw.isBirthday(2016, 6,0);
+        flag = hw.isBirthday(2016, 6,31);
+        flag = hw.isBirthday(2016, 7,0);
+        flag = hw.isBirthday(2016, 7,32);
+        flag = hw.isBirthday(2016, 8,0);
+        flag = hw.isBirthday(2016, 8,32);
+        flag = hw.isBirthday(2016, 9,0);
+        flag = hw.isBirthday(2016, 9,31);
+        flag = hw.isBirthday(2016, 10,0);
+        flag = hw.isBirthday(2016, 10,32);
+        flag = hw.isBirthday(2016, 11,0);
+        flag = hw.isBirthday(2016, 11,32);
+        flag = hw.isBirthday(2016, 12,0);
+        flag = hw.isBirthday(2016, 12,32);
+        flag = hw.isBirthday(1900, 2,29);
+        flag = hw.isBirthday(1900, 2,28);
+        flag = hw.isBirthday(2020, 0,1);
+        flag = hw.isBirthday(2020, 13,1);
+    }
+
+    @Test
+    public void testMiniCalculator(){
+        HelloWorld hw = new HelloWorld();
+        Double a = hw.miniCalculator(1,1,'+');
+        a = hw.miniCalculator(1,1,'-');
+        a = hw.miniCalculator(1,1,'*');
+        a = hw.miniCalculator(1,1,'/');
+        a = hw.miniCalculator(1,0,'+');
+        a = hw.miniCalculator(1,0,'-');
+        a = hw.miniCalculator(1,0,'*');
+        a = hw.miniCalculator(1,0,'/');
+        a = hw.miniCalculator(1,1,'=');
+    }
 }
